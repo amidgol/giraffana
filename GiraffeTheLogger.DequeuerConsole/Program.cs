2362edc35344ac4a9588bab3408f3521c2ef2362
amid <golmohammadi.edu@gmail.com>
2020-03-30 21:08:47 +0430

    Add dequeuer console

---
+++ GiraffeTheLogger.DequeuerConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GiraffeTheLogger.DequeuerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
