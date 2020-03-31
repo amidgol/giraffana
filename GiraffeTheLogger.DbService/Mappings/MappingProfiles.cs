0e1c0f9554ddd16281ffca836d732846a5329a37
amid <golmohammadi.edu@gmail.com>
2020-03-31 20:47:28 +0430

    Fix mapping

--- GiraffeTheLogger.DbService/Mappings/MappingProfiles.cs
+++ GiraffeTheLogger.DbService/Mappings/MappingProfiles.cs
@@ -4,15 +4,17 @@ using GiraffeTheLogger;
 using GiraffeTheLogger.Domain.Entities;
 using GiraffeTheLogger.Domain.Extensions;
 
-namespace GiraffeTheLogger.Domain.Mappings {
+namespace GiraffeTheLogger.DbService {
     public class MappingProfiles : Profile {
         public MappingProfiles () {
+
             CreateMap<MessageDto, LogEntity> ()
                 .ForMember (dest => dest.CreationDateTime,
                     opt => opt.MapFrom (src => src.CreationDateTime.ToUnixTimestampInSeconds ()))
                 .ForMember (dest => dest.ReceiveDateTime,
-                    opt => opt.MapFrom (src => DateTime.Now.ToUnixTimestampInSeconds ()));
-
+                    opt => opt.MapFrom (src => DateTime.Now.ToUnixTimestampInSeconds ()))
+                .ForMember (dest => dest.Id, src => src.Ignore ())
+                .ForMember (dest => dest.ReceiveDateTime, src => src.Ignore ());
         }
     }
 }
